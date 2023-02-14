402354c26d3a4ffdc6fa6d32aa50bb2a95e7fca9
fiatjaf <fiatjaf@gmail.com>
2023-02-14 07:40:15 -0300

    make nostrica banner responsive.

--- src/components/NostricaBanner.jsx
+++ src/components/NostricaBanner.jsx
@@ -1,21 +1,23 @@
 export function NostricaBanner() {
   return (
-    <div className="static top-0 right-0 left-0 z-10 flex h-12 items-center space-x-2 bg-violet-500 p-2 px-8 text-slate-300 lg:justify-evenly">
-      <a
-        href="https://nostrica.com/"
-        target="_blank"
-        className="block font-bold text-slate-100"
-      >
-        nostrica.com
-      </a>
-      <p>Unconference dedicated to bringing Nostriches together!</p>
-      <p>Uvita & Online</p>
-      <p>March 19-21</p>
-      <a href="https://nostrica.com/" target="_blank" className="block">
+    <div className="static top-0 right-0 left-0 z-10 flex h-12 items-center justify-evenly bg-violet-500 p-2 px-8 text-slate-300">
+      <a href="https://nostrica.com/" target="_blank">
         <img
           className="h-16"
           src="https://nostrica.com/images/nostrica-unconference.png"
         />
+      </a>
+      <p className="hidden font-bold text-cyan-300 lg:block">
+        Unconference dedicated to bringing Nostriches together!
+      </p>
+      <p className="hidden sm:block">Uvita & Online</p>
+      <p>March 19-21</p>
+      <a
+        href="https://nostrica.com/"
+        target="_blank"
+        className="block hidden font-bold text-white md:block"
+      >
+        nostrica.com
       </a>
     </div>
   )
